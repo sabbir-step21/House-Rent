d4cba0950af02de7911621979c845adfec42638f
sabbir-step21 <sabbir.step21@gmail.com>
2026-03-22 17:43:20 +0600

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,10 @@
 const CACHE_NAME = 'house-rent-v1';
 const ASSETS_TO_CACHE = [
   '/',
-  '/index.html',
-  '/src/main.tsx',
-  '/src/App.tsx',
-  '/src/index.css',
+  '../index.html',
+  '../src/main.tsx',
+  '../src/App.tsx',
+  '../src/index.css',
   '/manifest.json'
 ];
 
